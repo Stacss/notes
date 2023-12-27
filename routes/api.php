c275dcf1c0464a81d489fa4cb82697badb323e53
Stas <postnikov.sa@ya.ru>
2023-12-27 23:49:02 +0700

    add new routs

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\NoteController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Api\AuthApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('notes', NoteController::class);
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [AuthApiController::class, 'login']);
+
