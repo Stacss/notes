12a3460736ba19aa020debf8a68a3807ab533217
Stas <postnikov.sa@ya.ru>
2023-12-27 23:49:30 +0700

    add swagger docs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      title="Notes - тестовый проект",
+ *      version="1.0.0",
+ *      description="API управления заметками",
+ *      @OA\Contact(
+ *          email="postnikov.sa@ya.ru",
+ *          name="Stanislav"
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests;
