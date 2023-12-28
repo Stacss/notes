52a221ca2e3f8c09642210dffe6de3852a9d9f60
Stas <postnikov.sa@ya.ru>
2023-12-28 13:28:11 +0700

    add sanctum authorizations for notes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthApiController;
-use App\Http\Controllers\Api\NoteController;
+use App\Http\Controllers\Api\NoteApiController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('notes', NoteController::class);
+Route::middleware('auth:sanctum')->resource('notes', NoteApiController::class);
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [AuthApiController::class, 'login']);
