6d3d23941876651b1dbd9114846fbb6894b5a25a
Stas <postnikov.sa@ya.ru>
2023-12-28 22:45:48 +0700

    add descriptions

--- app/Http/Controllers/Web/NoteController.php
+++ app/Http/Controllers/Web/NoteController.php
@@ -19,6 +19,13 @@ class NoteController extends Controller
         $this->noteUpdateService = $noteUpdateService;
     }
 
+    /**
+     * Обновляет существующую заметку.
+     *
+     * @param \Illuminate\Http\Request $request Запрос, содержащий данные для обновления заметки.
+     * @param int $id Идентификатор (ID) существующей заметки, которую необходимо обновить.
+     * @return \Illuminate\Http\JsonResponse JSON-ответ с результатом операции обновления заметки.
+     */
     public function update(Request $request, $id)
     {
         $validatedData = $request->validate([
@@ -29,6 +36,12 @@ class NoteController extends Controller
         return $this->noteUpdateService->update($id, $validatedData);
     }
 
+    /**
+     * Создает новую заметку через AJAX-запрос.
+     *
+     * @param \Illuminate\Http\Request $request Запрос, содержащий данные для создания заметки.
+     * @return \Illuminate\Http\JsonResponse JSON-ответ с результатом операции создания новой заметки.
+     */
     public function storeAjax(Request $request)
     {
         $validatedData = $request->validate([
@@ -38,6 +51,13 @@ class NoteController extends Controller
 
         return $this->noteUpdateService->store($validatedData);
     }
+
+    /**
+     * Создает новую заметку на основе данных из запроса.
+     *
+     * @param \Illuminate\Http\Request $request Запрос, содержащий данные для создания заметки.
+     * @return \Illuminate\Http\RedirectResponse Перенаправляет на домашнюю страницу с уведомлением о успешном создании заметки.
+     */
     public function store(Request $request)
     {
         $validatedData = $request->validate([
@@ -57,6 +77,11 @@ class NoteController extends Controller
         return redirect()->route('home')->with('status', 'Заметка успешно создана');
     }
 
+    /**
+     * Отображает форму для создания новой заметки.
+     *
+     * @return \Illuminate\View\View Представление для создания новой заметки.
+     */
     public function create()
     {
         return view('note.create');
